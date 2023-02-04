3f0e3d77d44a582a0991001e993baf23fdfad2b8
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-02-04 17:39:14 -0500

    Type to a more specific type

--- wikidata_bot_framework/constants.py
+++ wikidata_bot_framework/constants.py
@@ -20,6 +20,6 @@ archive_url_prop = "P1065"
 archive_date_prop = "P2960"
 deprecated_reason_prop = "P2241"
 link_rot_id = "Q1193907"
-site = pywikibot.Site("wikidata", "wikidata")
+site: pywikibot.DataSite = pywikibot.Site("wikidata", "wikidata")
 
 EntityPage = Union[pywikibot.ItemPage, pywikibot.PropertyPage, pywikibot.LexemePage]
