6a90d85d80e004a495771e3a14b2a1ca72ee4231
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2024-01-26 18:09:26 -0500

    Add user-config

---
+++ user-config.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+import sys
+
+mylang = "wikidata"
+family = "wikidata"
+usernames = defaultdict(defaultdict)
+
+usernames[family][mylang] = "RPI2026F1Bot"
+
+simulate = "--simulate" in sys.argv
+
+password_file = "user-password.py"
+
+put_throttle = 1
+
+del defaultdict
