cb59758e70a83bd2d797917a0c9f3485fdbfd68e
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2026-03-18 14:09:26 -0400

    Rename user config

--- user-config-sample.py
+++ user-config-sample.py
@@ -5,11 +5,7 @@ mylang = "wikidata"
 family = "wikidata"
 usernames: defaultdict[str, defaultdict[str, str]] = defaultdict(defaultdict)
 
-usernames[family][mylang] = "RPI2026F1Bot"
-
 simulate = "--simulate" in sys.argv
-
-password_file = "user-password.py"
 
 put_throttle = 1
 
