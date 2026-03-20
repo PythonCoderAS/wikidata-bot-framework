cd11e2f0f05c580d9d1c892640ff2e8d79fd584e
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2026-03-20 01:00:41 -0400

    fix: Fix version detection if package isn't installed

--- wikidata_bot_framework/__version__.py
+++ wikidata_bot_framework/__version__.py
@@ -1,4 +1,8 @@
-from importlib.metadata import version as get_version
+from importlib.metadata import version as get_version, PackageNotFoundError
 
-__version__ = get_version(__package__)
+try:
+    __version__ = get_version(__package__)
+except PackageNotFoundError:
+    # This can be hit during tests
+    __version__ = "8.0.0"
 version_info = tuple(__version__.split("."))
